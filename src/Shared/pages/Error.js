6a8e767c3edbb2989286a48999eacc0677322850
camilo pinto <capir99@gmail.com>
2024-03-19 22:42:00 -0500

    error_page

--- src/Shared/pages/Error.js
+++ src/Shared/pages/Error.js
@@ -1,11 +1,43 @@
+import React from "react";
+import logo from "../../assets/logo404.PNG"; // Importa tu logo aquí
 
 const Error = () => {
+  return (
+    <div style={styles.container}>
+      <img src={logo} alt="Logo" style={styles.logo} />
+      <div style={styles.content}>
+        <h1 style={styles.heading}>Error 404</h1>
+        <p style={styles.message}>
+          Lo sentimos, la página que buscas no se ha encontrado.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    return(
-        <div>
-             <h1>Página de error en construcción.....</h1>
-        </div>
-    );
-}
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    height: "100vh",
+    backgroundColor: "#f8f9fa",
+  },
+  logo: {
+    width: 350,
+    marginBottom: 20,
+  },
+  content: {
+    textAlign: "center",
+  },
+  heading: {
+    fontSize: 36,
+    marginBottom: 10,
+  },
+  message: {
+    fontSize: 18,
+  },
+};
 
 export default Error;
